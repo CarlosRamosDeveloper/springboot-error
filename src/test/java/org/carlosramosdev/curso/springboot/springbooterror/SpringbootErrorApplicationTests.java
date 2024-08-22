f039b867fe37e177799b2edfb966b4649b8c8456
CarlosRamosDeveloper <CarlosRamosDev@protonmail.com>
2024-08-23 00:12:43 +0200

    initial commit

---
+++ src/test/java/org/carlosramosdev/curso/springboot/springbooterror/SpringbootErrorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.carlosramosdev.curso.springboot.springbooterror;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootErrorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
