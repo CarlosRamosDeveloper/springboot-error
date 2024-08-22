e568fd1123f4b00165888aa2ee8b0137215529b8
CarlosRamosDeveloper <CarlosRamosDev@protonmail.com>
2024-08-23 00:31:12 +0200

    dividedByZero now returns a custom error

--- src/main/java/org/carlosramosdev/curso/springboot/springbooterror/controllers/HandlerExceptionController.java
+++ src/main/java/org/carlosramosdev/curso/springboot/springbooterror/controllers/HandlerExceptionController.java
@@ -1,16 +1,25 @@
 package org.carlosramosdev.curso.springboot.springbooterror.controllers;
 
 import org.carlosramosdev.curso.springboot.springbooterror.models.Error;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Date;
 
 @RestControllerAdvice
 public class HandlerExceptionController {
 
     @ExceptionHandler(ArithmeticException.class)
-    public ResponseEntity<?> dividedByZero(Exception e){
+    public ResponseEntity<Error> dividedByZero(Exception e){
+        Error error = new Error();
+        error.setDate(new Date());
+        error.setError("Error: No se puede dividir por cero!");
+        error.setMessage(e.getMessage());
+        error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
 
-        return ResponseEntity.internalServerError().body("Error 500");
+//        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(error);
+        return ResponseEntity.internalServerError().body(error);
     }
 }
