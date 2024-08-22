65cbafd53d1968f978cc339eebfa6a7beebd8d57
CarlosRamosDeveloper <CarlosRamosDev@protonmail.com>
2024-08-23 01:02:47 +0200

    Implemented a new NumberFormatException

--- src/main/java/org/carlosramosdev/curso/springboot/springbooterror/controllers/HandlerExceptionController.java
+++ src/main/java/org/carlosramosdev/curso/springboot/springbooterror/controllers/HandlerExceptionController.java
@@ -4,10 +4,13 @@ import org.carlosramosdev.curso.springboot.springbooterror.models.Error;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class HandlerExceptionController {
@@ -26,7 +29,6 @@ public class HandlerExceptionController {
 
     @ExceptionHandler(NoHandlerFoundException.class)
     public ResponseEntity<Error> notFoundException(NoHandlerFoundException nhfe){
-
         Error error = new Error();
         error.setDate(new Date());
         error.setError("Api rest no encontrado!");
@@ -34,5 +36,17 @@ public class HandlerExceptionController {
         error.setStatus(HttpStatus.NOT_FOUND.value());
 
         return ResponseEntity.status(404).body(error);
+    }
+
+    @ExceptionHandler(NumberFormatException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, Object> numberFormatException(NumberFormatException nfe){
+        Map<String, Object> error = new HashMap<>();
+        error.put("date", new Date());
+        error.put("error", "Número no válido o error de formato");
+        error.put("message", nfe.getMessage());
+        error.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+
+        return error;
     }
 }
